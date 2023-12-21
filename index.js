53ff2ec47bea88ed5ae79f67cb27fecadc10d601
shiko <sarah.tharao@student.moringaschool.com>
2023-12-21 16:06:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+ let result
+  if (ride<=400){
+  result='freeSample';
+  }
+  return 'This one is on me!'
+}else if (distance > 400 && distance <= 2000) {
+  return 'I will gladly take your thirty bucks.';
+} else if (distance > 2000 && distance <= 2500) {
+  return '$30 charge for distances over 2000 feet.';
+} else {
+  return 'Rides over 2500 feet are not allowed.';
 }
-
 function ternaryCheckCity(){
-  // Write your code here!
+  return city==="NYC" ?'okay, sounds good.' :'No go.';
+console.log(ternaryCheckCity("LA"))
 }
-
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(money){
+    case "generous"
+    return "Thank you so much";
+    break;
+    case "not as generous":
+    return "Thank you.";
+    break;
+    default:
+      return "Bye."
+  }
 }
